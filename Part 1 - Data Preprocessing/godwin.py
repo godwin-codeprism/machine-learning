3ef7b8a450651ce3db24ca174435423ab2e67ea3
godwinvc <godwin@swift.local>
2017-10-24 18:35:02 +0530

    added Label encoder and onehotencoder

--- Part 1 - Data Preprocessing/godwin.py
+++ Part 1 - Data Preprocessing/godwin.py
@@ -11,5 +11,17 @@ X = dataset.iloc[:,:-1].values
 Y = dataset.iloc[:,3].values
 
 from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy='mean',axis = 0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
 
-imputer = Imputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_x = LabelEncoder()
+X[:,0] = labelEncoder_x.fit_transform(X[:,0])
+
+oneHotEncode = OneHotEncoder(categorical_features=[0])
+X = oneHotEncode.fit_transform(X).toarray()
+
+labelEncoder_y = LabelEncoder()
+Y = labelEncoder_y.fit_transform(Y)
+
